540f81dd4e9b35ee2f4843fdf1d53ceb1faa063d
Dexter Fort <dexterkun16@gmail.com>
2022-11-19 01:26:40 +0800

    Working except positive/negtive key

--- main.js
+++ main.js
@@ -2,51 +2,82 @@ let calc_key = document.querySelectorAll('.calc_key');
 
 var num1 = '';
 var num2 = '';
-var op;
-var result;
+var op = '';
+var result = '';
 
 // console.log(num1)
 
 calc_key.forEach(el => {
     el.addEventListener('click', () => {
-        if(el.dataset.key == 'num' && op == null) {
+        if(el.dataset.key == 'num' && op == '') {
             num1 += el.value;
-            console.log('num1 ' + num1)
+            console.log(`
+                num1 = ${num1}
+                num2 = ${num2}
+                op = ${op}
+                result = ${result}
+            `)
         }
         else if(el.dataset.key == 'op') {
             op = el.value
-            console.log(op)
+            console.log(`
+                num1 = ${num1}
+                num2 = ${num2}
+                op = ${op}
+                result = ${result}
+            `)
         }
-        else if(el.dataset.key == 'num' && num1 != '' && op != null) {
+        else if(el.dataset.key == 'num' && num1 != '' && op != '') {
             num2 += el.value;
-            console.log('num2 ' + num2)
+            console.log(`
+                num1 = ${num1}
+                num2 = ${num2}
+                op = ${op}
+                result = ${result}
+            `)
         }
         else if(el.dataset.key == 'backsp') {
-            if(num1 != '' && result == null) {
+            if(num1 != '' && result == '') {
                 num1 = num1.substring(0, num1.length - 1)
-                console.log('num1 ' + num1)
+                console.log(`
+                num1 = ${num1}
+                num2 = ${num2}
+                op = ${op}
+                result = ${result}
+            `)
             }
-            else if(num2 == '' && result == null) {
+            else if(num2 == '' && result == '') {
                 num2 = num2.substring(0, num2.length - 1)
-                console.log('num2 ' + num2)
+                console.log(`
+                num1 = ${num1}
+                num2 = ${num2}
+                op = ${op}
+                result = ${result}
+            `)
             }
             else{
                 result = String(result);
                 result = result.substring(0, result.length - 1)
-                console.log(result)
+                console.log(`
+                num1 = ${num1}
+                num2 = ${num2}
+                op = ${op}
+                result = ${result}
+            `)
             }
         }
         else if(el.dataset.key == 'clr') {
-            num1 = null;
-            num2 = null;
-            op = null;
+            num1 = '';
+            num2 = '';
+            op = '';
+            result = ''
             console.log('cleared')
         }
-        else if(el.dataset.key == 'equals' && num2 != '' && num1 != '' && op != null) {
+        else if(el.dataset.key == 'equals' && num2 != '' && num1 != '' && op != '') {
             num1 = Number(num1);
             num2 = Number(num2);
             
-            if(result != null) {
+            if(result != '') {
                 result = Number(result);
                 switch (op){
                     case '+':
@@ -65,7 +96,12 @@ calc_key.forEach(el => {
                         result = 'Syntax Error';
                     break;
                 }
-                console.log(result);
+                console.log(`
+                num1 = ${num1}
+                num2 = ${num2}
+                op = ${op}
+                result = ${result}
+            `)
             }
             else {
                 switch (op){
@@ -85,7 +121,12 @@ calc_key.forEach(el => {
                         result = 'Syntax Error';
                     break;
                 }
-                console.log(result);
+                console.log(`
+                num1 = ${num1}
+                num2 = ${num2}
+                op = ${op}
+                result = ${result}
+            `)
             }
             // num1 = ''
             num2 = ''
