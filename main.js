750fc7be9f1ed7f4e362d199f2e6b63884e7d7cf
Dexter Fort <dexterkun16@gmail.com>
2022-11-19 14:30:56 +0800

    Added keyboard support

--- main.js
+++ main.js
@@ -175,3 +175,110 @@ calc_key.forEach(el => {
         display();
     })
 })
+
+document.body.onkeydown = (e) => {
+    if(e.key == 1 || e.key == 2 || e.key == 3 || e.key == 4 || e.key == 5 || e.key == 6 || e.key == 7 || e.key == 8 || e.key == 9 || e.key == 0 || e.key == '.'){
+        if(op == '') {
+            num1 += e.key;
+        }
+        else if(num1 != '' && op != ''){
+            num2 += e.key;
+            result = '';
+        }
+        else {
+            num3 += e.key;
+        }
+    }
+    else if((result != '' || num1 != '') && ((e.key == '+' || e.key == '-' || e.key == '*' || e.key == '/'))) {
+        op = e.key
+        if(e.key == '+' || e.key == '-' || e.key == '*' || e.key == '/'){
+        }
+    }
+    else if(e.keyCode == 8) {
+        if(num1 != '' && op == '') {
+            num1 = num1.substring(0, num1.length - 1)
+        }
+        else if(num2 != '') {
+            num2 = num2.substring(0, num2.length - 1)
+        }
+        else if(num3 != '') {
+            num3 = num3.substring(0, num3.length -1)
+            console.log(num3)
+        }
+        else{
+            result = String(result);
+            result = result.substring(0, result.length - 1)
+        }
+        console.log(e.key);
+    }
+    else if(e.keyCode == 13 && op != '') {
+        console.log(e.key);
+        num1 = Number(num1);
+        num2 = Number(num2);
+        num3 = Number(num3);
+        
+        if(result != '') {
+            result = Number(result);
+            switch (op){
+                case '+':
+                    result = result + num3;
+                break;
+                case '-':
+                    result = result - num3;
+                break;
+                case '*':
+                    result = result * num3;
+                break;
+                case '/':
+                    result = result / num3;
+                break;
+                default:
+                    result = 'Syntax Error';
+                break;
+            }
+            console.log(`
+            num1 = ${num1}
+            num2 = ${num2}
+            num3 = ${num3}
+            op = ${op}
+            result = ${result}
+        `)
+        }
+        else {
+            switch (op){
+                case '+':
+                    result = num1 + num2;
+                break;
+                case '-':
+                    result = num1 - num2;
+                break;
+                case '*':
+                    result = num1 * num2;
+                break;
+                case '/':
+                    result = num1 / num2;
+                break;
+                default:
+                    result = 'Syntax Error';
+                break;
+            }
+            console.log(`
+            num1 = ${num1}
+            num2 = ${num2}
+            num3 = ${num3}
+            op = ${op}
+            result = ${result}
+        `)
+        }
+
+        if(result == Infinity) {
+            result = 'Cannot be divided by 0';
+        }
+
+        num1 = ''
+        num2 = ''
+        num3 = ''
+        op = ''
+    }
+    display();
+}
